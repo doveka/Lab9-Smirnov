665137f011932f5b877812ba66ec0b0f93d5b06e
doveka <131176206+doveka@users.noreply.github.com>
2023-12-28 00:22:10 +0200

    Add files via upload

---
+++ ConsoleApp20/Program.cs
@@ -0,0 +1,14 @@
+﻿int CalculateTotalMinutes(int hours, int minutes)
+{
+    int totalMinutes = hours * 60 + minutes;
+    return totalMinutes;
+}
+
+
+//Вы можете вызвать эту функцию и передать ей значения для полей 1 и 2, чтобы получить общее количество минут:
+
+
+int hours = 2;
+int minutes = 30;
+int totalMinutes = CalculateTotalMinutes(hours, minutes);
+Console.WriteLine("Общее количество минут: " + totalMinutes);
